7eb1e2bf0c170f3aefb97acff41c5a187f86ab69
Guilherme Silva <gui.ghs.silva@gmail.com>
2025-04-11 20:27:49 -0300

    Criando alerta caso o usuário não preencha os dados pedidos

--- main.js
+++ main.js
@@ -2,4 +2,13 @@
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
+
+    // Verifica se os campos foram preenchidos
+    if (!loginEmail || !loginSenha){
+        alert("Favor, preencher todos os campos de acesso!");
+    }
+
+    else{
+        
+    }
 }
